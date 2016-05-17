6ee91461cd45227125d51707a6be5402a23f00d7
gonzasestopal <gonzarosestopal@hotmail.com>
2016-05-17 09:36:40 -0500

    crsf cookie js

--- corbis/static/js/app.js
+++ corbis/static/js/app.js
@@ -20,6 +20,21 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
+
+function sameOrigin(url) {
+    // test that a given url is a same-origin URL
+    // url could be relative or scheme relative or absolute
+    var host = document.location.host; // host + port
+    var protocol = document.location.protocol;
+    var sr_origin = '//' + host;
+    var origin = protocol + sr_origin;
+    // Allow absolute or scheme relative URLs to same origin
+    return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+        // or any other URL that isn't scheme relative or absolute i.e relative.
+        !(/^(\/\/|http:|https:).*/.test(url));
+}
+
 $.ajaxSetup({
     beforeSend: function(xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
@@ -56,7 +71,7 @@ $('body').on('click', '.glyphicon-remove', function() {
 
 	$.ajax({
 		type: 'post',
-		url: 'productos/delete_product/',
+		url: "//"+location.host+"productos/delete_product/",
 		data: {pk: pk, csrfmiddlewaretoken: csrftoken},
 		success: function() {
 			$row.fadeOut(function() {$(this).remove()})
