be1003e8fbebbcc684beafe4884849e09ce7e874
gonzasestopal <gonzarosestopal@hotmail.com>
2016-05-17 09:39:23 -0500

    ensure csrf cookie

--- corbis/productos/views.py
+++ corbis/productos/views.py
@@ -9,7 +9,9 @@ from django.template import RequestContext
 from productos.forms import ProductoForm
 from productos.models import Producto
 
+from django.views.decorators.csrf import ensure_csrf_cookie
 
+@ensure_csrf_cookie
 def home(request):
 	context = RequestContext(request)
 	productos = Producto.objects.all()
